221403c3ad364751d0e3cc7fec3b549fb709d9c6
William Saunders <william@williamsaunders.net>
2017-05-29 22:09:28 -0400

    Use sharp 2Y0A02 distance sensor to avoid raising desk unless we need to

--- DeskController/DeskController.ino
+++ DeskController/DeskController.ino
@@ -8,35 +8,46 @@ unsigned long last_heartbeat = 0;
 const unsigned long WAIT_BEFORE_RAISE = 30000L; //30s
 const unsigned long RAISE_TIME = 15000L; //15s
 const unsigned long LOWER_TIME = 2500L; //2.5s
-bool raised = true;
+int sensorPin = A0;    // select the input pin for the potentiometer
+int sensorValue = 0;  // variable to store the value coming from the sensor
 
 
 // the setup function runs once when you press reset or power the board
 void setup() {
+  pinMode(up_pin, OUTPUT);
+  pinMode(down_pin, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(sensorPin, INPUT);  
+  
+  delay(1000);  
+  digitalWrite(LED_BUILTIN, HIGH);
+  raise();
+  digitalWrite(LED_BUILTIN, LOW);
+
   Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
   last_heartbeat = millis();
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(up_pin, OUTPUT);
-  pinMode(down_pin, OUTPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
+  while (Serial.available() > 0 && millis() - last_heartbeat < 1000) {
+    Serial.read();  // read and discard any input
+  }
   
-  delay(1000);  
-  raise();
+}
+
+boolean raised() {
+  return analogRead(sensorPin) < 100;
 }
 
 void raise() {
-    if(raised) {
+    if(raised()) {
       return;
     }
-    raised = true;  
-    digitalWrite(up_pin, HIGH);   // turn the LED on (HIGH is the voltage level)
+    digitalWrite(up_pin, HIGH);
     delay(RAISE_TIME);
     digitalWrite(up_pin, LOW);
     delay(1000);
-    digitalWrite(down_pin, HIGH);   // turn the LED on (HIGH is the voltage level)      
+    digitalWrite(down_pin, HIGH);
     delay(LOWER_TIME);
     digitalWrite(down_pin, LOW);      
 
@@ -54,13 +65,12 @@ void loop() {
       Serial.read();  // read and discard any input
     }
     last_heartbeat = millis();
-    raised = false;
     delay(300);
     digitalWrite(LED_BUILTIN, LOW);
     delay(300);
   } else {
     digitalWrite(LED_BUILTIN, LOW);
-    if(raised == false && millis() - last_heartbeat > WAIT_BEFORE_RAISE) {
+    if(raised() == false && millis() - last_heartbeat > WAIT_BEFORE_RAISE) {
       raise();
     }
     delay(300);
